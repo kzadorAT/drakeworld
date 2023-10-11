b7cfd2c819b8acea922b1edf160d72db8e059d29
Fabricio Loaiza <floaiza@silentiumapps.com>
2023-10-11 00:52:05 -0300

    Agregada implementacion basica para Spotify

--- src/App.jsx
+++ src/App.jsx
@@ -65,6 +65,17 @@ return (
             Se está reconstruyendo este sitio. Solo esperen un poco. Días en desarrollo: {dias}.
           </p>
         </section>
+        <section id="profile">
+          <h2>Este es mi Spotify <span id="displayName"></span></h2>
+          <span id="avatar"></span>
+          <ul>
+            <li>Id: <span id="id"></span></li>
+            <li>Email: <span id="email"></span></li>
+            <li>Spotify URI: <a id="uri" href="#"></a></li>
+            <li>Link: <a id="url" href="#"></a></li>
+            <li>Profile Image: <span id="imgUrl"></span></li>
+          </ul>
+        </section>
       </main>
     </div>
 );
